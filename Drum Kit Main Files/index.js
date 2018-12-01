eeffdea75876d1d2d9bd30ee280fe550edb8b03e
Carina Cruz <lecruz@bu.edu>
2018-12-01 03:49:03 +0000

    alert

--- Drum Kit Main Files/index.js
+++ Drum Kit Main Files/index.js
@@ -3,7 +3,7 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 for (var i = 0; i<numberOfDrumButtons; i++) {
     
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        alert("I got clicked!");
+        alert("I got clicked!"); //alert
 });
 
 }
