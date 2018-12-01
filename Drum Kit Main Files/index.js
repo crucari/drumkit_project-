59b08fa186fd654995009032a087adf7b33bce7b
Carina Cruz <lecruz@bu.edu>
2018-12-01 04:12:04 +0000

    adding the 1st sound

--- Drum Kit Main Files/index.js
+++ Drum Kit Main Files/index.js
@@ -3,7 +3,9 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 for (var i = 0; i<numberOfDrumButtons; i++) {
     
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        alert("I got clicked!"); //alert
+       
+       var audio = new Audio("sounds/tom-1.mp3");
+       audio.play();
 });
 
 }
